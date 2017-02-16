97df78e180c3a1ea9ac08d8acd4a6d4973e1cbee
kenca23 <kenca23@gmail.com>
2017-02-16 15:33:34 -0600

    Inserté dos archivos php para el registro e inicio de session de Android

---
+++ android/login.php
@@ -0,0 +1,28 @@
+<?php
+    try {
+        $connection = new MongoDB\Client;
+        $database = $connection->PuntosMuestreo;
+        $collection = $database->usuarios;
+    } catch (MongoConnectionException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    $correo = filter_var(strtolower($_POST['correo']), FILTER_SANITIZE_STRING);
+    $password = $_POST['contraseña'];
+    $password = hash('sha512', $password);
+    
+    $datos = $collection->findOne(array('$and' => array(array('correo' => $correo), array('password' => $password) )));
+
+    $response = array();
+    $response["success"] = false;
+
+    
+    if (!is_null($datos)) {
+        if($datos['validado'] == true){
+            $response["success"] = true;
+            $response["correo"] = $correo; 
+        }
+    }
+echo json_encode($response);
+
+?>
---
+++ android/registro.php
@@ -0,0 +1,42 @@
+<?php
+    try {
+        $connection = new MongoDB\Client;
+        $database = $connection->PuntosMuestreo;
+        $collection = $database->usuarios;
+    } catch (MongoConnectionException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
+    $nombre = filter_var($_POST['nombre'], FILTER_SANITIZE_STRING);
+    $correo = filter_var(strtolower($_POST['correo']), FILTER_SANITIZE_STRING);
+    $password = $_POST['contraseña'];
+    $password = hash('sha512', $password);
+    
+    $datos = $collection->findOne(array('correo' => $correo),array('correo' => 1));
+
+    $response = array();
+    $response["success"] = false;
+
+    
+    if (!is_null($datos)) {
+            $response["mensaje"] = "El correo ya existe"; 
+    }else{
+        try {
+            $documento = array();
+            $documento['nombre'] = $nombre;
+            $documento['correo'] = $correo;
+            $documento['password'] = $password;
+            $documento['validado'] = True;
+            $status = $collection->insertOne($documento, array('safe' => true));
+            $response["success"] = true;
+      
+        } catch(MongoCursorException $e){
+          $response["mensaje"] = "El registro falló"; 
+        } catch (MongoException $e){
+          $response["mensaje"] = "El registro falló"; 
+        }
+    }
+
+    echo json_encode($response);
+
+?>
