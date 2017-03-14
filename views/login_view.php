fcd3247239982bd2df3330271c54823b66d6725a
kenca23 <kenca23@gmail.com>
2017-03-14 01:04:18 -0600

    Se arregló el menú y se modificó el footer

--- views/login_view.php
+++ views/login_view.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Iniciar Sesión</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    
+
     <link rel="stylesheet" href="css/estilo_login.css">
 </head>
+
 <body>
-<div class="container">
-    <div class="row">
-        <div class='col-md-3'></div>
-        <div class="col-md-6">
-            <div class="login-box well">
-                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"  method= "POST" name="login">
+    <div class="container">
+        <div class="row">
+            <div class='col-md-3'></div>
+            <div class="col-md-6">
+                <div class="login-box well">
+                    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" name="login">
                         <legend>Iniciar Sesión</legend>
                         <div class="form-group">
                             <label for="username-email">E-mail</label>
@@ -24,7 +26,7 @@
                         <div class="form-group">
                             <label for="password">Contraseña</label>
                             <input id="password" name="contraseña" value='' placeholder="Contraseña" type="password" class="form-control" required pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="La contraseña debe de contener al menos 6 caracteres, incluyendo letras minúsculas, mayúsculas y números." onchange="this.setCustomValidity(this.validity.patternMismatch ? this.title : '');
-  if(this.checkValidity()) form.verificacion.pattern = this.value;"/>
+  if(this.checkValidity()) form.verificacion.pattern = this.value;" />
                         </div>
                         <div class="form-group">
                             <input class="btn btn-default btn-block m-t-md" type="submit" value="Iniciar Sesión">
@@ -32,31 +34,32 @@
                         <span class='text-center'><a href="recuperar.php" class="text-sm">No recuerdas la contraseña?</a></span>
                         <?php if(!empty($errores)): ?>
                             <div class="alert alert-danger" role="alert">
-                              <ul id="errores">
-                                <?php echo $errores; ?>
-                              </ul>
+                                <ul id="errores">
+                                    <?php echo $errores; ?>
+                                </ul>
                             </div>
-                        <?php endif; ?>
-                        <div class="form-group">
-                            <p class="text-center m-t-xs text-sm">No tienes cuenta?</p> 
-                            <a href="registro.php" class="btn btn-default btn-block m-t-md">Crea una cuenta</a>
-                        </div>
+                            <?php endif; ?>
+                                <div class="form-group">
+                                    <p class="text-center m-t-xs text-sm">No tienes cuenta?</p>
+                                    <a href="registro.php" class="btn btn-default btn-block m-t-md">Crea una cuenta</a>
+                                </div>
                     </form>
-                
+
+                </div>
             </div>
+            <div class='col-md-3'></div>
         </div>
-        <div class='col-md-3'></div>
     </div>
-</div>
 
     <!-- Latest compiled and minified CSS -->
     <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.1.1.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- views/menu_view.php
+++ views/menu_view.php
@@ -1,26 +1,50 @@
-	<!-- Menú principal -->
-  <nav class="navbar navbar-dark navbar-fixed-top bg-inverse margenNav" id="mainNav">
-			<button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-      <div class="collapse navbar-toggleable-md" id="navbarResponsive">
-      <a class="navbar-brand" href="index.php"><h3>Monitoreo</h3></a>
-      <div>
-      <ul class="nav navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="Insercion.php">Inserción</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="busqueda.php">Búsqueda <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Actualización</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Borrado</a>
-        </li>
-      </ul>
-      </div>
-      <form class="form-inline float-lg-right">
-          <input class="form-control btn btn-outline-primary" type="button" value="Entrar" onclick="location='login.php'"/>
-      </form>
+<!-- Menú principal -->
+<nav class="navbar navbar-dark navbar-fixed-top bg-inverse margenNav" id="mainNav">
+    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+    <div class="collapse navbar-toggleable-md" id="navbarResponsive">
+        <a class="navbar-brand" href="index.php"><h3>Monitoreo</h3></a>
+        <div>
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="Insercion.php">Inserción</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="busqueda.php">Búsqueda <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Actualización</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Borrado</a>
+                </li>
+
+
+
+            </ul>
+        </div>
+        <div class="form-inline float-lg-right menu_form">
+
+
+
+            <?php
+            if (!isset($_SESSION['correo'])) {
+                ?>
+
+                <a class="form-control btn btn-outline-primary " href="login.php">Iniciar Sesión</a>
+
+                <?php
+            }else{
+                ?>
+
+                    <span id="id_nomb_usuario" class="nombre_usuario">Hola, <?php echo ($_SESSION['correo']) ?></span>
+
+                    <a class="form-control btn btn-outline-primary" href="login.php">Cerrar Sesión</a>
+
+                    <?php
+            } 
+        ?>
+
+        </div>
+
     </div>
-		</nav>
+</nav>
