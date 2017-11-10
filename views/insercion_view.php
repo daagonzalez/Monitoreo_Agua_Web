f882bb625c24fef68c24f79585f4ae2808bd0b2d
johanduran <johan.durancerdas@gmail.com>
2017-11-10 00:35:26 -0600

    cambios

--- views/insercion_view.php
+++ views/insercion_view.php
@@ -325,7 +325,11 @@
                 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
                 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
-
+                <script type="text/javascript">
+                    window.onload = function() {
+                        document.body.style.marginTop = document.getElementById('mainNav').clientHeight + 10 + "px";
+                    };
+                </script>
 				<script type="text/javascript">
 					$(document).ready(
 						function(){
